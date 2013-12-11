122c6d91e3c858f32fb86536772e33f233c36e3c
Valeriy Aksonov <ax.valeriy@gmail.com>
2013-12-12 00:14:41 +0200

    add missing exclamation point

--- src/Sample/HelloWorld.java
+++ src/Sample/HelloWorld.java
@@ -1,10 +1,7 @@
 package Sample;
 
-/**
- * Created by valeriy on 11.12.13.
- */
 public class HelloWorld {
     public static String getMessage(){
-        return "Hello World";
+        return "Hello World!";
     }
 }
